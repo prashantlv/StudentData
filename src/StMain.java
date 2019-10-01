317515ead21b8122fe4fd9fa75ad831a17ee91be
Marshall <shukla.prashant689@gmail.com>
2019-10-02 01:24:33 +0530

    Add files via upload

---
+++ src/StMain.java
@@ -0,0 +1,21 @@
+
+public class StMain {
+
+	public static void main(String[] args) 
+	{
+		Student[] st=new Student[3];
+		
+		for(int i=0;i<3;i++)
+		{
+			st[i]=new Student();
+			st[i].input();
+				
+		}
+		for(int l=0;l<3;l++)
+		{
+			st[l].display();
+		}
+
+	}
+
+}
---
+++ src/Student.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Student
+ {
+	int rollno;
+	String name;
+	
+	Scanner sc=new Scanner(System.in);
+	double[] marks=new double[3];
+  public void input()
+{
+	  System.out.println("Enter name");
+	  name=sc.nextLine();
+	  System.out.println("Enter roll no.");
+	  rollno=sc.nextInt();
+	  System.out.println("Enter Phy. Chem. & Maths Marks res.");
+	  for(int j=0;j<3;j++)
+	  marks[j]= sc.nextDouble();
+}
+  public void display()
+  {
+	  System.out.println("Name : "+name);
+	  System.out.println("Roll no : "+rollno);
+	 // for(int k=0;k<3;k++)
+	 // System.out.println("Marks :"+marks[k]);
+	  System.out.println("Physics Marks :"+marks[0]);
+	  System.out.println("Chemistry Marks :"+marks[1]);
+	  System.out.println("Maths Marks :"+marks[2]);
+	  System.out.println("\n");
+  }
+}
